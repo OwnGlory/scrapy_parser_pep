d472353b608f0732ae15d2cc3c6b38c210dac778
Vlad_Work <inf.vlad.s@bk.ru>
2024-04-22 16:07:29 +0300

    Review changes

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,12 +1,12 @@
-import re
 import scrapy
 from pep_parse.items import PepParseItem
+from pep_parse.constant import ALLOWED_DOMAINS_PEP
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    allowed_domains = ALLOWED_DOMAINS_PEP
+    start_urls = ['https://' + allowed_domains[0]]
 
     def parse(self, response):
         for author_link in response.css('a[href^="pep-"]'):
@@ -14,22 +14,13 @@ class PepSpider(scrapy.Spider):
                 author_link, callback=self.parse_pep
             )
 
-    def parse_pep(self, response):
-        data = {
-            'number': (
-                re.search(
-                    r'PEP (\d{1,4})',
-                    response.css(
-                        'section#pep-content h1.page-title::text'
-                    ).get()
-                ).group(1)),
-            'name': (
-                re.search(
-                    r'PEP \d{1,4} – (.*)',
-                    response.css(
-                        'section#pep-content h1.page-title::text'
-                    ).get()
-                ).group(1)),
-            'status': response.css(('section#pep-content abbr::text')).get()
-        }
+    def parse_pep(self, response): 
+        title_text = response.css('section#pep-content h1.page-title::text').get()
+        title_parts = title_text.split(' – ')
+
+        data = { 
+            'number': title_parts[0].replace('PEP ', ''), 
+            'name': title_parts[1], 
+            'status': response.css('section#pep-content abbr::text').get() 
+        } 
         yield PepParseItem(data)
